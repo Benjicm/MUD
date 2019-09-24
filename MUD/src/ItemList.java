7abeed6249ca13fa668be8836581790b7c0e8557
ben <ben@10.160.182.143>
2019-09-24 13:37:38 -0400

    idk

--- MUD/src/ItemList.java
+++ MUD/src/ItemList.java
@@ -9,4 +9,37 @@ public class ItemList {
 		items = new ArrayList<Item>();
 		nextID = 0;
 	}
+	
+	private int getItemIndex(int itemID)
+	{
+		for(int i = 0; i < items.size(); i++)
+		{
+			if(items.get(i).getID() == itemID)
+			{
+				return i;
+			}
+		}
+		return 0;
+	}
+	public ArrayList<Integer> findItemByName(String itemName)
+	{
+		String name = itemName.toLowerCase();
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		for(int i = 0; i < items.size(); i++)
+		{
+			if(items.get(i).getName().equals(name))
+			{
+				a.add(items.get(i).getID());
+			}
+		}
+		return a;
+	}
+	public int addItem(String itemName, boolean carriable)
+	{
+		Item n = new Item(itemName, nextID, carriable);
+		nextID++;
+		items.add(n);
+		return n.getID();
+	}
+	
 }
