ffa6382721bdb7a214929139c009bf1dd9ce55f7
abarbee129 <abarbee2020@gmail.com>
2019-09-26 14:19:38 -0700

    this will screw things up hopefully

--- MUD/src/CharTester.java
+++ MUD/src/CharTester.java
@@ -3,13 +3,25 @@ import java.util.*;
 public class CharTester {
 	public static void main(String[] args)
 	{
-		GameMaster gm = new GameMaster();
-		gm.rlist.addRoom("You are in a dark, musty prison cell.");
-		gm.rlist.addExitToRoom(1, 2, Exit.NORTH);
-		gm.ilist.addItem("key", true);
-		gm.ilist.addItem("sword", true);
-		gm.rlist.addItem(1, 1);
-		gm.rlist.addItem(1, 2);
-		System.out.println(gm.getRoomDesc(1));
+		Character c = new Character();
+		c.addItem(1);
+		c.addItem(2);
+		c.addItem(3);
+		c.addItem(4);
+		c.addItem(5);
+		printInv(c);
+		System.out.println(c.holdingItem(3));
+		c.addItem(4);
+		c.removeItem(3);
+		printInv(c);
+		System.out.println(c.holdingItem(3));
+	}
+	public static void printInv(Character c)
+	{
+		ArrayList<Integer> inv = c.getInv();
+		for(int i = 0; i < inv.size(); i++)
+		{
+			System.out.println(inv.get(i));
+		}
 	}
 }
