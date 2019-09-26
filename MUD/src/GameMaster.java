708e8ec7acb5886602ffd1d775991d4e056070fa
ben <ben@10.160.84.13>
2019-09-26 14:22:27 -0400

    re-added getDesc

--- MUD/src/GameMaster.java
+++ MUD/src/GameMaster.java
@@ -1,8 +1,10 @@
+import java.util.*;
 
 public class GameMaster {
 
 	RoomList rlist;
 	ItemList ilist;
+	CharList clist;
 	boolean running = true;
 
 
@@ -21,7 +23,73 @@ public class GameMaster {
 		// the setup() method still needs something to initialize the player
 	}
 
-
+	private String getExitDesc(String dir)
+	{
+		if(dir.equals(Exit.NORTH))
+		{
+			return "There is an exit to the north.\n";
+		}
+		else if(dir.equals(Exit.NORTHEAST))
+		{
+			return "There is an exit to the northeast.\n";
+		}
+		else if(dir.equals(Exit.EAST))
+		{
+			return "There is an exit to the east.\n";
+		}
+		else if(dir.equals(Exit.SOUTHEAST))
+		{
+			return "There is an exit to the southeast.\n";
+		}
+		else if(dir.equals(Exit.SOUTH))
+		{
+			return "There is an exit to the south.\n";
+		}
+		else if(dir.equals(Exit.SOUTHWEST))
+		{
+			return "There is an exit to the southwest.\n";
+		}
+		else if(dir.equals(Exit.WEST))
+		{
+			return "There is an exit to the west.\n";
+		}
+		else if(dir.equals(Exit.NORTHWEST))
+		{
+			return "There is an exit to the northwest.\n";
+		}
+		else if(dir.equals(Exit.UP))
+		{
+			return "There is a staircase leading upwards.\n";
+		}
+		else if(dir.equals(Exit.DOWN))
+		{
+			return "There is a staircase leading downwards.\n";
+		}
+		return null;
+	}
+	public String getRoomDesc(int roomID)
+	{
+		String output = rlist.getRoomDesc(roomID) + "\n";
+		ArrayList<Integer> roomItems = rlist.getRoomItems(roomID);
+		for(int i = 0; i < roomItems.size(); i++)
+		{
+			String itemName = ilist.getItemName(roomItems.get(i));
+			String itemDesc = "There is a " + itemName + " on the ground.\n";
+			output = output + itemDesc;
+		}
+		ArrayList<Exit> roomExits = rlist.getRoomExits(roomID);
+		for(int i = 0; i < roomExits.size(); i++)
+		{
+			String exitDesc = getExitDesc(roomExits.get(i).getDir());
+			output = output + exitDesc;
+		}
+		return output;
+	}
+	public String getDescOfLocation(int charID)
+	{
+		int roomID = clist.getCharLocation(charID);
+		return this.getRoomDesc(roomID);
+	}
 	public void run() {
 		
 		InputManager in = new InputManager();
